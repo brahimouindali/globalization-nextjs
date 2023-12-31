f3267a7ed36a6cb212409c555136c7b377827ca3
brahim ouindali <ibrahim.ouindali@outlook.fr>
2023-12-31 16:16:10 +0100

    fix

--- middleware.ts
+++ middleware.ts
@@ -2,21 +2,5 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname;
-  const shouldHandleLocale = !(
-    pathname.startsWith("/_next") ||
-    pathname.startsWith("/api/") ||
-    pathname.includes(".", pathname.lastIndexOf("/"))
-  );
-
-  if (shouldHandleLocale) {
-    const countryUsingGEO = request.geo?.country ?? "en";
-
-    const url = request.nextUrl.clone();
-    url.locale = countryUsingGEO;
-
-    return NextResponse.rewrite(url);
-  }
-
   return NextResponse.next();
 }
